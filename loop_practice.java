bc6d4853349876fba9da672a5462058573349793
Aridro Chatterjee <aridrochatterjee@gmail.com>
2026-09-26 13:14:22 +0530

    feat: added more loop practices

--- loop_practice.java
+++ loop_practice.java
@@ -42,12 +42,42 @@ public class loop_practice{
         }
 
         System.out.println("--------------------------------    ");
-        
 
 
+        for (int i = 1 ; i <= n; i++){
+            for (int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------    ");
 
+        for (int i = 1 ; i <= 5; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+        System.out.println();
+        }
+        System.out.println("--------------------------------    ");
 
+        for (int i = 5 ; i >= 1; i--){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------    ");
+
+        int num = 1;
+        for (int i = 1 ; i <= 5; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(num);
+                num++;  
+            }
+            System.out.println();
+        }
     }
-
-
 }
